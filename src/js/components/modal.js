4721c86962ab9f051f95c6de7d5cca0695349457
Grigory <gregory.rvl@yandex.ru>
2023-02-26 18:40:24 +0600

    fixing manual coords

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -5,7 +5,10 @@ export default function createModal() {
   modal.innerHTML = `<h3 class="modal-header">Something went wrong</h3>
     <p class="modal-message">Please let us get your coordinates</p>
     <p class="modal-message">Latitude and longitude, separated by comma</p>
-    <input type"text" class="modal-input">`;
-
+    <input type"text" class="modal-input">
+    <div class="modal-buttons">
+      <button class="modal-ok button" type="button">Ok</button>
+      <button class="modal-cancel button" type="button">Отмена</button>
+    </div>`;
   return modal;
 }
